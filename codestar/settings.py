19293e504a5ed9300b1c02d838e242ebd02e2580
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-11-12 19:22:51 +0000

    remove secret key from settings py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6vs1866rs-%rg4kdwlb=xeai%7kzz)b%1op!l+x5#^+y$i)b9+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
