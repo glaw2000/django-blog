655c4a6b6a1a266b66cfbbde466127c0b22c3f3b
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-11-13 10:47:35 +0000

    add Post model to models file

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qb=(gk$lg3er@2(_@yg@&w&x6*es0t1@@1$)_mmem=2vsr=&cf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-glaw2000-djangoblog-lycufxihjvz.ws.codeinstitute-ide.net','.herokuapp.com']
 
